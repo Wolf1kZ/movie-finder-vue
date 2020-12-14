f40322c0cf35d91eb3cf61e6059c55bb3ee6a320
Konstantin <kostya.zelinsky@list.ru>
2020-12-14 21:20:26 +0600

    Initial commit

---
+++ src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1> You Picked The Wrong House Fool <a href="/">Get off! Now!</a></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<style scoped>
+
+</style>
